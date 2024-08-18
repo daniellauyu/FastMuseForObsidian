293051703d89ae5b53afa1b35f14972ded75ca1a
huixing <contact@liuyude.com>
2024-08-18 14:37:40 +0800

    封装版本

--- fastMuseForObsidian.py
+++ fastMuseForObsidian.py
@@ -11,36 +11,39 @@
 """
 
 import sys
+import os
 import datetime
 
-# 接收从Alfred传入的参数
-input_text = sys.argv[1] if len(sys.argv) > 1 else "默认文本"
+class JournalEntry:
+    def __init__(self):
+        # 从环境变量中获取配置
+        self.base_path = os.getenv('BASE_PATH', '/default/path/if/not/set')
+        self.inspiration_tag = os.getenv('INSPIRATION_TAG', '## 插入关键字')
+        self.diary_keyword = os.getenv('DIARY_KEYWORD', '日记关键词')
 
-# 设置文件目录和名称格式
-base_path = "/Users/daniellau/SynologyDrive/obsidian_nas/6-每日记录"
-date_today = datetime.datetime.now().strftime("%Y%m%d")  # 格式化日期
-file_name = f"碎片-{date_today}.md"
+        self.date_today = datetime.datetime.now().strftime("%Y%m%d")
+        self.file_name = f"{self.diary_keyword}-{self.date_today}.md"
+        self.full_path = f"{self.base_path}/{self.file_name}"
+        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# 完整的文件路径
-full_path = f"{base_path}/{file_name}"
+    def add_inspiration(self):
+        input_text = sys.argv[1] if len(sys.argv) > 1 else "默认文本"
+        text_to_add = f"- {self.timestamp} {input_text}\n"
 
-# 获取当前时间戳
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        try:
+            with open(self.full_path, 'r+', encoding='utf-8') as file:
+                content = file.readlines()
+                inspiration_index = next(i for i, line in enumerate(content) if self.inspiration_tag in line)
+                content.insert(inspiration_index + 1, text_to_add)  # 在灵感标题下方插入
+                file.seek(0)  # 回到文件开始
+                file.writelines(content)  # 写入修改后的内容
+                file.truncate()  # 移除旧内容中未被覆盖的部分
+            print("灵感已成功添加！")
+        except FileNotFoundError:
+            print("文件未找到，请检查文件路径和名称是否正确。")
+        except Exception as e:
+            print(f"发生错误：{e}")
 
-# 格式化要添加的文本，包含从Alfred传入的参数
-text_to_add = f"- {timestamp} {input_text}\n"
-
-# 读取并修改文件内容
-try:
-    with open(full_path, 'r+', encoding='utf-8') as file:
-        content = file.readlines()
-        inspiration_index = next(i for i, line in enumerate(content) if '## 灵感' in line)
-        content.insert(inspiration_index + 1, text_to_add)  # 在灵感标题下方插入
-        file.seek(0)  # 回到文件开始
-        file.writelines(content)  # 写入修改后的内容
-        file.truncate()  # 移除旧内容中未被覆盖的部分
-    print("灵感已成功添加！")
-except FileNotFoundError:
-    print("文件未找到，请检查文件路径和名称是否正确。")
-except Exception as e:
-    print(f"发生错误：{e}")
+if __name__ == "__main__":
+    journal_entry = JournalEntry()
+    journal_entry.add_inspiration()
